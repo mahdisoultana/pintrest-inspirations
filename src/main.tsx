0999d655b5c588c8577da05a7531418a83991b8a
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-02-22 14:15:44 +0100

    adding gsap alternative for animation

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import PageIdProvider from './components/context/RouteProvider';
-import RoutesApp from './components/router';
+import RoutesApp from './components/gsap/router';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
