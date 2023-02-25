a69150550d72b9d56cc1f88973b813e2df275c4a
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-02-25 16:41:37 +0100

    adding social media links to cards project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { motion, useCycle } from 'framer-motion';
 import { useState } from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import {
+  FaDribbble,
+  FaGithub,
+  FaInstagram,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { VscLiveShare } from 'react-icons/vsc';
 import Netflixvideo from './netflix/assets/Netflix demo animation.mp4';
 import netflixSrc from './netflix/assets/netflix-img.jpg';
-
 function App() {
   const [selected, setSelected] = useState(0);
   return (
@@ -14,20 +22,12 @@ function App() {
     </main>
   );
 }
-
 export default App;
 function Card() {
   const [sel, setSel] = useState(true);
   return (
-    <div className="  rounded-lg shadow-md overflow-hidden shadow-black/60  w-full relative h-[270px]">
-      {sel && (
-        <img
-          src={netflixSrc}
-          alt="netflix-img "
-          className="absolute top-0 object-cover left-0 w-full h-full bg-gray-900 z-10"
-          onClick={() => setSel(false)}
-        />
-      )}
+    <motion.div className="  rounded-lg shadow-md overflow-hidden shadow-black/60  w-full relative h-[270px]">
+      {sel && <OverlayCard setSel={setSel} />}
       <video
         onClick={() => setSel(true)}
         className="w-full h-full relative z-[1] "
@@ -35,6 +35,144 @@ function Card() {
       >
         <source src={Netflixvideo} />
       </video>
+    </motion.div>
+  );
+}
+type PropsType = {} & React.ComponentPropsWithoutRef<'button'>;
+function PlayButton(props: PropsType) {
+  return (
+    <button
+      title={'play video Preview ✅'}
+      {...props}
+      className="abs-center w-[50px] h-[50px] shadow hover:shadow-md duration-1000 shadow-black   rounded-full  z-10 flex items-center justify-center  hover:scale-[1.2]"
+    >
+      <motion.div whileHover={{ scale: 1.5 }} whileTap={{ scale: 1 }}>
+        <motion.div className="absolute w-full h-full top-0 left-0 group-hover:block hidden opacity-0  duration-700 ">
+          {[
+            { bg: 'bg-red-300/50' },
+            { bg: 'bg-orange-500/30' },
+            { bg: 'bg-blue-600/70' },
+          ].map((item, index) => (
+            <Bubble key={index} bg={item.bg} index={index} />
+          ))}
+        </motion.div>
+        <AiFillPlayCircle className="  rounded-full w-full h-full text-6xl relative text-gray-100 group-hover:text-blue-500 duration-1000" />
+      </motion.div>
+    </button>
+  );
+}
+
+function Bubble({ bg, index }: { index: number; bg: string }) {
+  const variantsItem = {
+    initial: {},
+    animate: (i: number) => {
+      const num = i + Math.random() * 1;
+
+      return {
+        opacity: [0, 1, 0],
+        scale: [0, 1.4, 2],
+        transition: {
+          repeat: Infinity,
+          duration: 6,
+          delay: num + -1,
+        },
+      };
+    },
+  };
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={variantsItem}
+      custom={index}
+      className={`w-[100%] h-[100%]  rounded-full absolute ${bg} `}
+    />
+  );
+}
+function OverlayCard({ setSel }: { setSel: (pram: boolean) => void }) {
+  const variantsC = {
+    animate: { opacity: 1, transition: { delay: 2, staggerChildren: 0.5 } },
+    initial: { opacity: 0 },
+  };
+
+  const [status, setState] = useCycle<'hover' | 'notHover'>(
+    'hover',
+    'notHover',
+  );
+  const animate = (dir = 1, anime?: any) => ({
+    opacity: status == 'notHover' ? 1 : 0,
+    x: status == 'notHover' ? '0%' : dir > 0 ? '-200%' : '200%',
+    transition: { delay: status == 'notHover' ? 0.4 : 0 },
+    ...anime,
+  });
+  const hoverEffect = {
+    rotate: 375,
+    y: [0, -5, 0],
+    transition: { duration: 0.5, delay: 0.3 },
+  };
+  return (
+    <div
+      className="group"
+      onMouseEnter={() => setState()}
+      onMouseLeave={() => setState()}
+    >
+      <img
+        src={netflixSrc}
+        alt="netflix-img "
+        className={`absolute top-0 object-cover left-0 w-full h-full z-10`}
+      />
+      <div
+        className={` absolute top-0 object-cover left-0 w-full h-full bg-gradient-to-l duration-1000 ${
+          status == 'hover'
+            ? 'to-black/0 from-transparent'
+            : 'from-black/80 to-transparent'
+        } z-10`}
+      />
+      <PlayButton onClick={() => setSel(false)} />
+      <motion.div
+        initial="initial"
+        animate="animate"
+        variants={variantsC}
+        className="absolute right-4 top-2 flex flex-col items-center justify-center space-y-4 z-[11]"
+      >
+        <motion.div
+          whileHover={hoverEffect}
+          animate={animate()}
+          title="Dribbble"
+        >
+          <FaDribbble className=" cursor-pointer  text-2xl z-10 text-pink-400 hover:text-pink-600" />
+        </motion.div>
+        <motion.div
+          whileHover={hoverEffect}
+          animate={animate(-1)}
+          title="LinkedinIn"
+        >
+          <FaLinkedinIn className=" cursor-pointer    text-2xl z-10 hover:text-blue-400  text-blue-500" />
+        </motion.div>
+        <motion.div
+          whileHover={hoverEffect}
+          animate={animate()}
+          title="Instagram"
+        >
+          <FaInstagram className=" cursor-pointer    text-2xl z-10 text-indigo-500 hover:text-red-400" />
+        </motion.div>
+        <motion.div
+          whileHover={hoverEffect}
+          animate={animate(-1)}
+          title="Github"
+        >
+          <FaGithub className=" cursor-pointer    text-2xl z-10 text-gray-600 hover:text-gray-100" />
+        </motion.div>
+        <motion.div
+          style={{ originY: '50%' }}
+          whileHover={hoverEffect}
+          animate={animate(-1)}
+          title="See The demo live ✨"
+          className=" mt-12 w-10 h-10 p-2 flex items-center justify-center"
+        >
+          <VscLiveShare className=" cursor-pointer   block text-2xl z-10  text-blue-300 hover:text-blue-800" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
