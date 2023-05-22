161bd8748fdd84ac7847dec44cfd9ae1e6663b30
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-05-22 20:33:09 +0100

    finishing paths ui and ready for animations path

--- src/projects/animateSVGShapesOnScroll/index.tsx
+++ src/projects/animateSVGShapesOnScroll/index.tsx
@@ -3,7 +3,7 @@ import CopyRightFooter from '../../component/CopyRightFooter';
 import './style.css';
 function AnimateSVGShapesOnScroll() {
   return (
-    <section className="bg-gray-100 text-black w-[99vw] min-h-screen overflow-hidden    font-Bodoni   flex items-center justify-center flex-col">
+    <section className="bg-gray-100 text-black w-full min-h-screen overflow-hidden    font-Bodoni   flex items-center justify-center flex-col">
       <Intro />
       <ImgShap src="/tea/2.jpg" />
       <TwoColumnContent />
@@ -15,17 +15,28 @@ function AnimateSVGShapesOnScroll() {
       <ImgShapWithText className="text-white" />
       <Chinise />
       <section className=" max-w-[1200px] w-full m-auto relative  ">
-        <ImgShap imgClipPath={true} src="/tea/6.jpg" />
         <ImgShap
           imgClipPath={true}
-          // src="/tea/.jpg"
-          className=" absolute top-1/2 -translate-y-1/2 left-0 w-full "
+          src="/tea/6.jpg"
+          className="   w-full h-screen "
         />
-        <ImgShap imgClipPath={true} src="/tea/1.jpg" />
+        <ImgShap
+          src="/tea/2.jpg"
+          imgClipPath={true}
+          className=" absolute top-1/2 -translate-y-1/2 left-0 w-full h-screen "
+        />
+        <ImgShap
+          imgClipPath={true}
+          src="/tea/1.jpg"
+          className=" w-full h-screen "
+        />
       </section>
       <TwoColumnContentStart />
 
-      <Sperial className="w-[100vw] bg-black flex items-center justify-center border" />
+      <Sperial
+        colorPath="white"
+        className="w-[100vw] bg-black flex items-center justify-center  "
+      />
       <div className="bg-black w-[100vw]">
         <CopyRightFooter
           className="py-20 text-orange-400  text-sm font-Nunito bg-black w-[100vw]"
@@ -49,17 +60,44 @@ function Chinise() {
 function GridColumns() {
   return (
     <section className="grid lg:grid-cols-2 m-auto max-w-[700px] w-[100vw] gap-x-14 mt-[100px]">
-      {[1, 2, 3, 4].map((i) => (
-        <div
-          className={`h-[450px] w-full  border border-white/20 bg-gray-900 ${
-            i % 2 == 0 && 'lg:mt-[150px]'
-          }`}
-        ></div>
-      ))}
+      {[1, 2, 3, 4].map((i) => {
+        const classLeft = 'w-[500px] top-0 left-[-160px] absolute';
+        const classRight = 'w-[500px] top-0 right-[-170px] absolute';
+        return (
+          <div
+            className={`h-[450px] w-full relative  border border-white/20 bg-gray-900 ${
+              i % 2 == 0 && 'lg:mt-[150px]'
+            }`}
+          >
+            <SVGPath
+              height="100%"
+              className={i % 2 == 0 ? classRight : classLeft}
+              curvePath={
+                i % 2 == 0 ? PortraitcurvePathRight : PortraitcurvePathLeft
+              }
+              straightPath={
+                i % 2 == 0 ? PortraitcurvePathLeft : PortraitcurvePathRight
+              }
+            />
+            <SVGPath
+              height="100%"
+              className={i % 2 == 0 ? classLeft : classRight}
+              curvePath={
+                i % 2 == 0 ? PortraitcurvePathLeft : PortraitcurvePathRight
+              }
+              straightPath={
+                i % 2 == 0 ? PortraitcurvePathRight : PortraitcurvePathLeft
+              }
+            />
+          </div>
+        );
+      })}
     </section>
   );
 }
 function TwoColumnContent() {
+  const classLeft = 'w-[500px] top-0 left-[-155px] absolute';
+  const classRight = 'w-[500px] top-0 right-[-167px] absolute';
   return (
     <section className=" m=auto my-10 max-w-[1000px] lg:p-4 w-full mt-10 grid lg:grid-cols-2  min-h-screen   text-right text-lg  ">
       <div className="px-12">
@@ -78,6 +116,18 @@ function TwoColumnContent() {
         </p>
       </div>
       <div className="w-full   relative  lg:h-full  h-screen ">
+        <SVGPath
+          height="100%"
+          className={classRight}
+          curvePath={PortraitcurvePathRight}
+          straightPath={PortraitcurvePathLeft}
+        />
+        <SVGPath
+          height="100%"
+          className={classLeft}
+          curvePath={PortraitcurvePathLeft}
+          straightPath={PortraitcurvePathRight}
+        />
         <img
           className="  w-full h-full block object-cover absolute top-0 left-0"
           src="/tea/5.jpg"
@@ -87,10 +137,32 @@ function TwoColumnContent() {
   );
 }
 function TwoColumnContentStart() {
+  const classLeft = ' top-0 left-[-155px] absolute';
+  const classRight = ' top-0 right-[-167px] absolute';
   return (
     <div className="bg-black w-[100vw] mt-20">
       <section className="m-auto max-w-[1000px] mt-10 grid lg:grid-cols-2  min-h-screen  text-right text-lg mb-10 items-center">
         <div className="w-full h-full relative">
+          <SVGPath height="100vh" colorPath="black" />
+          <SVGPath
+            colorPath="black"
+            className="absolute bottom-0 left-0 w-full h-[200px]  "
+            curvePath="M -1 768 h 1366 c 0 0 -187 -184 -431 -187 C 499 574 587 716 -1 768 z"
+          />
+          <SVGPath
+            colorPath="black"
+            height="100%"
+            className={classRight}
+            curvePath={PortraitcurvePathRight}
+            straightPath={PortraitcurvePathLeft}
+          />
+          <SVGPath
+            colorPath="black"
+            height="100%"
+            className={classLeft}
+            curvePath={PortraitcurvePathLeft}
+            straightPath={PortraitcurvePathRight}
+          />
           <img
             className="w-full h-full block object-cover absolute top-0 left-0"
             src="/tea/3.jpg"
@@ -150,22 +222,20 @@ function ImageClipPath({
     <svg
       className={`z-[200] pointer-events-none h-[${height}]  flex items-center justify-center ${className} `}
       version="1.1"
-      id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       x="0px"
       y="0px"
       viewBox="0 0 1366 768"
-      xmlSpace="preserve"
       preserveAspectRatio="none"
     >
       <defs>
         <clipPath id="shape1">
           <path
-            data-to="M0,0c445.1,56.9,900.9,51,1366,0v768
-	c-446.5-53.9-901.5-56.1-1366,0V0z"
-            d="M0.5,229.5c423.9,233.8,881.9,228.8,1365,0l1,296
-	c-441.8,317.1-900.9,298.2-1366,5L0.5,229.5z"
+            data-anim-to="M0.5,524.5c455.7,322.7,911,315.6,1366-7L1366,173
+            C910.7,571.6,455.3,571.6,0,173L0.5,524.5z"
+            d="M0.5,246.5c455.7-322.7,911-315.6,1366,7L1366,598
+            C910.7,199.3,455.3,199.3,0,598L0.5,246.5z"
           />
         </clipPath>
       </defs>
@@ -210,16 +280,15 @@ function ImgShap({
   );
 }
 function SVGPath({
+  colorPath = 'fill-gray-100',
   straightPath = straithPath,
   curvePath = curvePath1,
-  height = '',
+  height = '100vh',
   className = 'absolute top-0 left-0 w-full  ',
 }) {
   return (
     <svg
-      className={`z-[200] pointer-events-none ${
-        height || ' h-[60vh]'
-      } ${className} block`}
+      className={`z-[200] pointer-events-none h-[${height}] ${className} block`}
       version="1.1"
       id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
@@ -232,7 +301,7 @@ function SVGPath({
       xmlSpace="preserve"
       preserveAspectRatio="none"
     >
-      <path d={curvePath} className="fill-gray-100" />
+      <path d={curvePath} className={colorPath} />
     </svg>
   );
 }
@@ -244,8 +313,24 @@ function ImgShapWithText({
 }) {
   return (
     <article
-      className={` bg-gray-900 relative min-h-screen w-[100vw]  ${className}`}
+      className={` bg-gray-900 relative min-h-screen w-[100vw]   ${className}`}
     >
+      {/* M0,0c0,0,1375.5-8.5,1366,0s-209.7,166.5-433.5,177.5
+      C709.5,188.5,514.5,31.5,0,0z*/}
+      <SVGPath
+        straightPath="M1366-3.8C1366-3.8-9.5-6.5,0-3.3s209.7,63.1,433.5,67.2
+	C656.5,68,851.5,8.3,1366-3.8z"
+        curvePath="M0,0c0,0,1375.5-8.5,1366,0s-209.7,166.5-433.5,177.5
+      C709.5,188.5,514.5,31.5,0,0z"
+      />
+      <SVGPath
+        straightPath="M1399,774.1c0,0-1408.7,8.8-1398.9,0
+	c9.7-8.8,78.8-75.4,201.4-95.6c225-37,446.2,76.5,564,77c261,1,295.7-87.3,442-69C1279.5,695.5,1299.6,767.9,1399,774.1z"
+        className="bottom-0 left-0 absolute"
+        curvePath="M1399,774.1c0,0-1408.7,8.8-1398.9,0
+	c9.7-8.8,259.5-184.7,387.6-188.6c100.2-3,263.2,118.9,381,118.3c100.2-0.6,200.9-136.8,347.2-118.3
+	C1265.5,604.5,1299.6,767.9,1399,774.1z"
+      />
       <img
         src={src}
         alt="tea"
@@ -258,17 +343,77 @@ function ImgShapWithText({
     </article>
   );
 }
-function Sperial({ className = 'bg-gray-900' }) {
+function Sperial({ className = ' ', flip = false, colorPath = '#260404' }) {
   return (
     <div
-      className={`min-h-screen flex items-center justify-center border w-[100vw] text-white p-10 ${className}`}
+      className={`min-h-screen   w-[100vw] text-white relative  overflow-hidden ${
+        flip ? 'rotate-180' : 'rotate-0'
+      } ${className}`}
     >
-      {' '}
-      sperial Here{' '}
+      <SVGSperial colorPath={colorPath} />
+      <SVGSperial
+        top="top-[12rem]"
+        path="M-35.5,384.5c4.1-13.5,12.3-32.4,30-44
+	c45.2-29.6,95.1,26.6,186,39c121.6,16.6,163.3-66.2,278-33c45.7,13.2,35,25.2,91,45c123.1,43.6,239.8,9,259,3
+	c97.9-30.5,103-72.8,167-68c53.4,4,57,33.9,151,61c35.4,10.2,79.1,22.3,135,17c52.5-5,93.1-23.2,118-37"
+        colorPath={colorPath}
+      />
+      <SVGSperial
+        top="top-[-5rem]"
+        path="M-34.4,363.4c3.5,17.9,10.9,42.9,28,58.6
+	c43.9,40.2,96.2-31.6,187.6-44.7c122.3-17.5,160.3,92.2,276.4,52.7c46.2-15.7,36.1-31.7,93-55.8c125-52.9,240.1-3.6,259,4.9
+	c96.5,43.2,99.6,98.8,163.9,94.7c53.5-3.4,58.5-42.5,153.6-74.7c35.8-12.1,80-26.5,135.7-17.6c52.2,8.3,92,33.6,116.3,52.4"
+        colorPath={colorPath}
+      />
+      <SVGSperial
+        top="top-[-10rem]"
+        path="M-35.5,384.5c4.1-13.5,12.3-32.4,30-44
+	c45.2-29.6,95.1,26.6,186,39c121.6,16.6,163.3-66.2,278-33c45.7,13.2,35,25.2,91,45c123.1,43.6,239.8,9,259,3
+	c97.9-30.5,103-72.8,167-68c53.4,4,57,33.9,151,61c35.4,10.2,79.1,22.3,135,17c52.5-5,93.1-23.2,118-37"
+        colorPath={colorPath}
+      />
     </div>
+  );
+}
+function SVGSperial({
+  top = 'top-0',
+  path = `M-33.5,334.5c12.9,26.1,36,62.7,73,71
+	c78.5,17.6,135.6-110.6,209-95c71.9,15.3,105.4,143.8,186,160c51.5,10.3,128.6-105.5,172-121c47.4-16.9,118.9-14.3,153,16
+	c46.1,41,33.3,79.5,75,112c37.6,29.3,83.8,25.9,137,22c44.3-3.3,170-12.5,267-102c37.3-34.4,63.9-74.7,106-72
+	c22.1,1.4,39.5,14.1,51,25`,
+  colorPath = '#260404',
+}) {
+  return (
+    <svg
+      className={`z-[200] absolute  left-0  pointer-events-none h-[100vh] w-full block ${top}`}
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="100%"
+      height="100%"
+      viewBox="0 0 1366 768"
+      xmlSpace="preserve"
+      preserveAspectRatio="none"
+    >
+      <path
+        fill="none"
+        stroke={colorPath}
+        stroke-width="2"
+        stroke-miterlimit="10"
+        d={path}
+      />
+    </svg>
   );
 }
 export default AnimateSVGShapesOnScroll;
 
 const straithPath = 'M0 768h1366H0z';
 const curvePath1 = 'M0 0h1366S792.5 137.5 474.5 137.5C201.5 137.5 0 0 0 0z';
+
+const PortraitcurvePathRight = `M911.2,782c0,0,6.2-788.2-0.3-782.7S784.8,119.4,776.4,247.7
+	C768.1,375.5,887.2,487.2,911.2,782z`;
+const PortraitcurvePathLeft = `M419.9-26c0,0-4.4,806.5,0.3,801c4.7-5.6,91.6-123,97.6-254.2
+	C523.8,390,437.4,275.7,419.9-26z`;
