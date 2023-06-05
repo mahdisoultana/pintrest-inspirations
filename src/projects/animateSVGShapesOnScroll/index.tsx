b0e349fb1ada16eaed2f71ab5edc71d7d29416af
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-06-05 13:09:26 +0100

    aniateSvgShapes completed 🎁

--- src/projects/animateSVGShapesOnScroll/index.tsx
+++ src/projects/animateSVGShapesOnScroll/index.tsx
@@ -1,7 +1,16 @@
+import Lenis from '@studio-freight/lenis';
 import { motion } from 'framer-motion';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
 import CopyRightFooter from '../../component/CopyRightFooter';
 import './style.css';
+gsap.registerPlugin(ScrollTrigger);
+
 function AnimateSVGShapesOnScroll() {
+  useEffect(() => {
+    scrollBase();
+  }, []);
   return (
     <section className="bg-gray-100 text-black w-full min-h-screen overflow-hidden    font-Bodoni   flex items-center justify-center flex-col">
       <Intro />
@@ -143,13 +152,24 @@ function TwoColumnContentStart() {
     <div className="bg-black w-[100vw] mt-20">
       <section className="m-auto max-w-[1000px] mt-10 grid lg:grid-cols-2  min-h-screen  text-right text-lg mb-10 items-center">
         <div className="w-full h-full relative">
-          <SVGPath height="100vh" colorPath="black" />
           <SVGPath
+            start="top 80%"
+            end="center 0%"
+            height="100vh"
+            colorPath="black"
+            straightPath="M 0 0 h 1366 S 869 2 490 -2 C 203 6 0 0 0 0 z"
+          />
+          <SVGPath
+            start="top 80%"
+            end="center 0%"
             colorPath="black"
             className="absolute bottom-0 left-0 w-full h-[200px]  "
             curvePath="M -1 768 h 1366 c 0 0 -187 -184 -431 -187 C 499 574 587 716 -1 768 z"
+            straightPath="M -1 768 h 1366 c 0 0 -203 -4 -469 -1 C 503 763 593 761 -1 768 z"
           />
           <SVGPath
+            start="top 80%"
+            end="center 0%"
             colorPath="black"
             height="100%"
             className={classRight}
@@ -157,6 +177,8 @@ function TwoColumnContentStart() {
             straightPath={PortraitcurvePathLeft}
           />
           <SVGPath
+            start="top 80%"
+            end="center 0%"
             colorPath="black"
             height="100%"
             className={classLeft}
@@ -218,8 +240,29 @@ function ImageClipPath({
   className = '',
   src = '/tea/1.jpg',
 }) {
+  const animPath = `M 0.5 246.5 c 528.5 153.5 827.5 144.5 1366 7 L 1366 598 C 826 722 539 734 0 598 L 0.5 246.5 z`;
+  const svgRef = useRef<any>(null);
+  const pathRef = useRef<any>(null);
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: svgRef.current,
+
+          // markers: true,
+          scrub: 2,
+          start: 'top center',
+        },
+      })
+      .to(pathRef.current, {
+        attr: {
+          d: animPath,
+        },
+      });
+  }, []);
   return (
     <svg
+      ref={svgRef}
       className={`z-[200] pointer-events-none h-[${height}]  flex items-center justify-center ${className} `}
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
@@ -232,10 +275,8 @@ function ImageClipPath({
       <defs>
         <clipPath id="shape1">
           <path
-            data-anim-to="M0.5,524.5c455.7,322.7,911,315.6,1366-7L1366,173
-            C910.7,571.6,455.3,571.6,0,173L0.5,524.5z"
-            d="M0.5,246.5c455.7-322.7,911-315.6,1366,7L1366,598
-            C910.7,199.3,455.3,199.3,0,598L0.5,246.5z"
+            ref={pathRef}
+            d="M 0.5 246.5 c 490.5 -147.5 844.5 -162.5 1366 7 L 1366 598 C 821 426 498 446 0 598 L 0.5 246.5 z"
           />
         </clipPath>
       </defs>
@@ -264,7 +305,12 @@ function ImgShap({
       className={` bg-gray-900 w-full h-screen overflow-hidden    ${className}`}
     >
       {/* top */}
-      <SVGPath height="100vh" />
+      <SVGPath2
+        height="100vh"
+        curvePath="M-0.5,0c298.4,224.2,480,230.7,591,194.3c124.4-40.8,200.2-148.6,339-127.3c105.1,16.2,140.1,90,232,83.9
+	c89.7-6,158.5-83.1,204-150.9H-0.5z"
+        straightPath="M-0.5,0c298.4,0,480,0,591,0c124.4,0,200.2,0,339,0c105.1,0,140.1,0,232,0c89.7,0,158.5,0,204,0H-0.5z"
+      />
       {/* bottom */}
 
       <img
@@ -272,22 +318,45 @@ function ImgShap({
         src={src}
         alt="tea"
       />
-      <SVGPath
+      <SVGPath2
+        straightPath="M-0.5,768.5c298.4-5.8,480-6,591-5.1c124.4,1.1,200.2,3.9,339,3.3c105.1-0.4,140.1-2.3,232-2.2
+	c89.7,0.2,158.5,2.2,204,3.9H-0.5z"
         className="absolute bottom-0 left-0 w-full h-[400px]  "
-        curvePath="M -1 768 h 1366 c 0 0 -187 -184 -431 -187 C 499 574 587 716 -1 768 z"
+        curvePath="M-0.5,768.5c298.4-227.3,480-233.9,591-197c124.4,41.4,200.2,150.6,339,129c105.1-16.4,140.1-91.2,232-85
+	c89.7,6.1,158.5,84.3,204,153H-0.5z"
       />
     </article>
   );
 }
-function SVGPath({
+function SVGPath2({
   colorPath = 'fill-gray-100',
   straightPath = straithPath,
   curvePath = curvePath1,
   height = '100vh',
   className = 'absolute top-0 left-0 w-full  ',
 }) {
+  const svgRef = useRef<any>(null);
+  const pathRef = useRef<any>(null);
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: svgRef.current,
+          start: 'top 80%',
+          end: 'center 20%',
+          // markers: true,
+          scrub: 2,
+        },
+      })
+      .to(pathRef.current, {
+        attr: {
+          d: straightPath,
+        },
+      });
+  }, []);
   return (
     <svg
+      ref={svgRef}
       className={`z-[200] pointer-events-none h-[${height}] ${className} block`}
       version="1.1"
       id="Layer_1"
@@ -301,10 +370,74 @@ function SVGPath({
       xmlSpace="preserve"
       preserveAspectRatio="none"
     >
-      <path d={curvePath} className={colorPath} />
+      <path ref={pathRef} d={curvePath} className={colorPath} />
     </svg>
   );
 }
+function SVGPath({
+  colorPath = 'fill-gray-100',
+  straightPath = straithPath,
+  curvePath = curvePath1,
+  height = '100vh',
+  className = 'absolute top-0 left-0 w-full  ',
+  start = 'center bottom',
+  end = 'center top',
+}) {
+  const svgRef = useRef<any>(null);
+  const pathRef = useRef<any>(null);
+  let animPath = '';
+  if (PortraitcurvePathRight === curvePath) {
+    animPath =
+      'M 911.2 782 c 0 0 6.2 -788.2 -0.3 -782.7 S 1066 112 1069 275 C 1050 439 887.2 487.2 911.2 782 z';
+  } else if (PortraitcurvePathLeft === curvePath) {
+    animPath =
+      'M 419.9 -26 c 0 0 -4.4 806.5 0.3 801 c 4.7 -5.6 1.8 -128 -2.2 -265 C 417 368 426 275 419.9 -26 z';
+  } else {
+    animPath = straightPath;
+  }
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: svgRef.current,
+          start,
+          end,
+          // markers: true,
+          scrub: 0.3,
+        },
+      })
+      .to(pathRef.current, {
+        attr: {
+          d: animPath,
+        },
+      });
+  }, []);
+  return (
+    <svg
+      ref={svgRef}
+      className={`z-[200] pointer-events-none h-[${height}] ${className} block`}
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="100%"
+      height="100%"
+      viewBox="0 0 1366 768"
+      xmlSpace="preserve"
+      preserveAspectRatio="none"
+    >
+      <path
+        ref={pathRef}
+        d={curvePath}
+        data-path-anim=""
+        className={colorPath}
+      />
+    </svg>
+  );
+}
+
 function ImgShapWithText({
   className = '',
   src = '/tea/2.jpg',
@@ -318,14 +451,16 @@ function ImgShapWithText({
       {/* M0,0c0,0,1375.5-8.5,1366,0s-209.7,166.5-433.5,177.5
       C709.5,188.5,514.5,31.5,0,0z*/}
       <SVGPath
-        straightPath="M1366-3.8C1366-3.8-9.5-6.5,0-3.3s209.7,63.1,433.5,67.2
-	C656.5,68,851.5,8.3,1366-3.8z"
+        start="30% 80%"
+        end="bottom 0%"
+        straightPath="M 0 0 c 0 0 1375.5 -8.5 1366 0 s -199 -5 -442 -5 C 680 -2 520 -5 0 0 z"
         curvePath="M0,0c0,0,1375.5-8.5,1366,0s-209.7,166.5-433.5,177.5
       C709.5,188.5,514.5,31.5,0,0z"
       />
       <SVGPath
-        straightPath="M1399,774.1c0,0-1408.7,8.8-1398.9,0
-	c9.7-8.8,78.8-75.4,201.4-95.6c225-37,446.2,76.5,564,77c261,1,295.7-87.3,442-69C1279.5,695.5,1299.6,767.9,1399,774.1z"
+        start="top 80%"
+        end="bottom 0%"
+        straightPath="M 1399 774.1 c 0 0 -1408.7 8.8 -1398.9 0 c 18.9 -0.1 271.9 0.9 410.9 2.9 c 146 -2 196 -1 382 0 c 100.2 -0.6 143 1 321 -4 C 1303 777 1303 774 1399 774.1 z"
         className="bottom-0 left-0 absolute"
         curvePath="M1399,774.1c0,0-1408.7,8.8-1398.9,0
 	c9.7-8.8,259.5-184.7,387.6-188.6c100.2-3,263.2,118.9,381,118.3c100.2-0.6,200.9-136.8,347.2-118.3
@@ -350,26 +485,31 @@ function Sperial({ className = ' ', flip = false, colorPath = '#260404' }) {
         flip ? 'rotate-180' : 'rotate-0'
       } ${className}`}
     >
-      <SVGSperial colorPath={colorPath} />
+      <SVGSperial
+        colorPath={colorPath}
+        path="M0,369c34.1-29,88.1-65.3,150-59c70.7,7.1,85.1,63,159,74c85.1,12.6,99.8-56.4,198-56
+	c103.1,0.4,135.8,76.7,215,53c50.7-15.2,55.8-51.9,103-53c54.7-1.3,65.2,47.6,120,49c62.2,1.5,82.2-60.8,135-51
+	c43.2,8,44.7,52.6,85,56c49.6,4.2,76.4-61,113-50c30.1,9,30.8,58.7,52,60c11.7,0.7,28.1-13.4,50-73"
+      />
       <SVGSperial
         top="top-[12rem]"
-        path="M-35.5,384.5c4.1-13.5,12.3-32.4,30-44
-	c45.2-29.6,95.1,26.6,186,39c121.6,16.6,163.3-66.2,278-33c45.7,13.2,35,25.2,91,45c123.1,43.6,239.8,9,259,3
-	c97.9-30.5,103-72.8,167-68c53.4,4,57,33.9,151,61c35.4,10.2,79.1,22.3,135,17c52.5-5,93.1-23.2,118-37"
+        path="M0,369c34.1-29,88.1-65.3,150-59c70.7,7.1,85.1,63,159,74c85.1,12.6,99.8-56.4,198-56
+	c103.1,0.4,135.8,76.7,215,53c50.7-15.2,55.8-51.9,103-53c54.7-1.3,65.2,47.6,120,49c62.2,1.5,82.2-60.8,135-51
+	c43.2,8,44.7,52.6,85,56c49.6,4.2,76.4-61,113-50c30.1,9,30.8,58.7,52,60c11.7,0.7,28.1-13.4,50-73"
         colorPath={colorPath}
       />
       <SVGSperial
         top="top-[-5rem]"
-        path="M-34.4,363.4c3.5,17.9,10.9,42.9,28,58.6
-	c43.9,40.2,96.2-31.6,187.6-44.7c122.3-17.5,160.3,92.2,276.4,52.7c46.2-15.7,36.1-31.7,93-55.8c125-52.9,240.1-3.6,259,4.9
-	c96.5,43.2,99.6,98.8,163.9,94.7c53.5-3.4,58.5-42.5,153.6-74.7c35.8-12.1,80-26.5,135.7-17.6c52.2,8.3,92,33.6,116.3,52.4"
+        path="M0,369c34.1-29,88.1-65.3,150-59c70.7,7.1,85.1,63,159,74c85.1,12.6,99.8-56.4,198-56
+	c103.1,0.4,135.8,76.7,215,53c50.7-15.2,55.8-51.9,103-53c54.7-1.3,65.2,47.6,120,49c62.2,1.5,82.2-60.8,135-51
+	c43.2,8,44.7,52.6,85,56c49.6,4.2,76.4-61,113-50c30.1,9,30.8,58.7,52,60c11.7,0.7,28.1-13.4,50-73"
         colorPath={colorPath}
       />
       <SVGSperial
         top="top-[-10rem]"
-        path="M-35.5,384.5c4.1-13.5,12.3-32.4,30-44
-	c45.2-29.6,95.1,26.6,186,39c121.6,16.6,163.3-66.2,278-33c45.7,13.2,35,25.2,91,45c123.1,43.6,239.8,9,259,3
-	c97.9-30.5,103-72.8,167-68c53.4,4,57,33.9,151,61c35.4,10.2,79.1,22.3,135,17c52.5-5,93.1-23.2,118-37"
+        path="M7,367c26.6-31.2,65.3-66.1,108-62c50.9,4.9,57.3,60.4,109,72c77,17.3,119.4-93.3,205-83
+	c69.7,8.4,80.8,86.5,142,83c63.1-3.6,81.6-88.4,138-84c46.6,3.6,53,62.8,103,67c49.7,4.2,61.1-52.8,116-58
+	c74.5-7.1,109.6,92.6,171,80c53-10.9,63.8-92.7,100-89c37.6,3.8,50,94.4,73,92c23.3-2.4,23.3-96.5,45-99c11.9-1.4,27.8,24.8,49,90"
         colorPath={colorPath}
       />
     </div>
@@ -383,8 +523,35 @@ function SVGSperial({
 	c22.1,1.4,39.5,14.1,51,25`,
   colorPath = '#260404',
 }) {
+  const animPath = `M0,393c33.5-55,84.2-120.7,141-117c84.7,5.5,116.1,161.9,188,158c70.3-3.8,93.3-156.2,155-154
+	c59.5,2.1,81.3,145.2,132,143c53-2.3,68.1-160.3,118-162c54-1.8,84.2,181.5,135,179c48-2.4,65.3-168.3,102-167
+	c38.3,1.3,50.8,183.3,86,184c34.7,0.7,53.5-175.4,89-175c36,0.4,52.6,181.7,83,181c30.6-0.7,37.6-185.9,70-188
+	c20.9-1.3,44.4,73.9,67,177`;
+
+  const svgRef = useRef<any>(null);
+  const pathRef = useRef<any>(null);
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: svgRef.current,
+          start: 'center bottom',
+          end: 'center top',
+          // markers: true,
+          scrub: 0.4,
+        },
+      })
+      .to(pathRef.current, {
+        // opacity: 0,
+        attr: {
+          d: animPath,
+        },
+      });
+  }, []);
+
   return (
     <svg
+      ref={svgRef}
       className={`z-[200] absolute  left-0  pointer-events-none h-[100vh] w-full block ${top}`}
       version="1.1"
       id="Layer_1"
@@ -399,6 +566,7 @@ function SVGSperial({
       preserveAspectRatio="none"
     >
       <path
+        ref={pathRef}
         fill="none"
         stroke={colorPath}
         stroke-width="2"
@@ -417,3 +585,17 @@ const PortraitcurvePathRight = `M911.2,782c0,0,6.2-788.2-0.3-782.7S784.8,119.4,7
 	C768.1,375.5,887.2,487.2,911.2,782z`;
 const PortraitcurvePathLeft = `M419.9-26c0,0-4.4,806.5,0.3,801c4.7-5.6,91.6-123,97.6-254.2
 	C523.8,390,437.4,275.7,419.9-26z`;
+
+function scrollBase() {
+  const lenis = new Lenis({
+    lerp: 0.1,
+    smooth: true,
+  });
+
+  function raf(time: any) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
